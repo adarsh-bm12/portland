dafff53e9134a558c73c0bb702cd06a0007f50c8
adarshbm11 <65062532+adarshb.ram36@users.noreply.gitlab.com>
2021-03-29 04:13:14 +0530

    c

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("PortaSwitch®_Architecture_and_Concepts/Copyright_notice.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('PortaSwitch®_Architecture_and_Concepts/Copyright_notice.htm'));
 rh.consts('START_FILEPATH', encodeURI('PortaSwitch®_Architecture_and_Concepts/Copyright_notice.htm'));
-rh.consts('HELP_ID', '8dc46be1-dd37-4dab-b28e-24b4cc163cc9' || 'preview');
+rh.consts('HELP_ID', '330fc75a-e2fe-43a5-b52e-4b7ed9342442' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");
